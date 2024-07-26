1d16876d561e8c80e472998c523509301d572236
sathishh2003 <sathishhh2003@gmail.com>
2024-07-27 01:07:34 +0530

    add some images for branches and add contactus page and and did some styling

--- index.js
+++ index.js
@@ -11,4 +11,41 @@ menu.onclick = () => {
         menu.classList.toggle('bx-x');
         navbar.style.left = '50%';
         navbar.classList.toggle('open');
+
     };
+
+   /*  contactUs */
+
+const btn = document.getElementById("btn");
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const mobile = document.getElementById('mobile');
+
+btn.addEventListener('click',()=>{
+
+    
+
+    if(name.value === ""){
+        name.style.border = "2px solid red";
+    }
+    else if(mobile.value === ""){
+        mobile.style.border = "2px solid red";
+    }
+    else if(email.value === ""){
+        email.style.border = "2px solid red";
+    }
+    else{
+    window.location.href = 'index.html';
+    }
+
+    if(name.value !== ""){
+        name.style.border = "none";
+    }
+    if(mobile.value !== ""){
+        mobile.style.border = "none";
+    }
+    if(email.value !== ""){
+        email.style.border = "none";
+    }
+});
+
